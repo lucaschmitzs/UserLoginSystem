8ab345457ffe76ccff72245e26dc213ae39b7743
Lucas Schmitz <lucaschmitzsantos@gmail.com>
2021-02-13 02:16:48 -0300

    melhorias

--- user/models.py
+++ user/models.py
@@ -16,15 +16,19 @@ class User:
         user = {
             "_id": uuid.uuid4().hex,
             "name": request.form.get('name'),
+            "username": request.form.get('username'),
             "email": request.form.get('email'),
             "password": request.form.get('password')
         }
+
         # Encrypt the password
         user['password'] = pbkdf2_sha256.encrypt(user['password'])
 
         # Check for existing email addres
         if db.users.find_one({"email": user['email']}):
             return jsonify({"error": "Email already in use"}), 400
+        elif db.users.find_one({"username": user['username']}):
+            return jsonify({"error": "Username already in use"}), 400
 
         if db.users.insert_one(user):
             return self.start_session(user)
@@ -37,10 +41,13 @@ class User:
 
     def login(self):
         user = db.users.find_one({
-            "email": request.form.get('email')
+            "username": request.form.get('username')
         })
 
         if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
             return self.start_session(user)
 
         return jsonify({"error": "Invalid login credentials"}), 401
+
+    def update(self):
+        db.users.find_one_and_update({"username": session['user']['username']})
